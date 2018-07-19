5929f8111552c1a8677ec865c421488147ac0c0e
Neel Mehta <neelmehta96@gmail.com>
2018-07-19 14:47:35 -0400

    show players

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,12 @@
     <!-- <img src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 
-    Sup
+    <ul id="example-1">
+      <li v-for="player in players">
+        {{ player.name }} #{{ player.number }}
+      </li>
+    </ul>
+
   </div>
 </template>
 
@@ -11,8 +16,20 @@
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
 
+// load data
+import players from "@/data/players.json";
+
+console.log(players);
+
 export default {
   name: "home",
+
+  data(){
+    return {
+      players: players
+    }
+  },
+
   components: {
     // HelloWorld
   }
