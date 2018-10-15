40ce5cdf4d6ad33807bcad2fb1b0291d53f33f48
Neel Mehta <neelmehta96@gmail.com>
2018-10-15 07:20:24 -0700

    adjust

--- src/models/HalfInning.js
+++ src/models/HalfInning.js
@@ -42,8 +42,69 @@ export default class HalfInning {
     Vue.set(this.baserunners, number, runner);
   }
 
-  advanceRunners(numBases) {
-    // moves all
+  clearBase(number) {
+    this.setBaserunner(number, null);
+  }
+
+  /**
+    Moves whoever is on `from` to `to` (base numbers).
+    Does not check if someone is already on the `to` base!!
+    Does not check if you went past home
+  */
+  moveBaserunner(from, to) {
+    let runner = this.baserunners[from]
+    this.setBaserunner(to, runner)
+    this.clearBase(from)
+  }
+
+  /**
+    Advances the baserunner at base number `from` the given number of bases.
+    checks if you went home or further, in which case it awards runs.
+  */
+  advanceBaserunner(from, numBases) {
+
+  }
+
+  advanceAllRunners(numBases) {
+    if (numBases === 0) {
+      return
+    }
+
+    // moves all runners up a certain number of bases
+    // does not include forcing; this is primarily used for hitting
+    // and errors and balks. not walks.
+    // move them all from 3rd base forward
+    if (this.baserunners[2]) {
+      // third base
+      // we are guaranteed that numBases >= 1
+      // so anybody on 3rd will score no matter what
+      this.clearBase(2)
+      this.runs++
+    }
+    if (this.baserunners[1]) {
+      // second base
+      if (numBases >= 2) {
+        // this person auto-scores w/ a double or bigger
+        this.clearBase(1)
+        this.runs++
+      }
+      else {
+        // just advancing one base - from 2nd (1) to 3rd (2)
+        // dumb zero-indexing
+        this.moveBaserunner(1, 2)
+      }
+    }
+    if (this.baserunners[0]) {
+      if (numBases >= 3) {
+        // a triple or better scores this person
+        this.clearBase(0)
+        this.runs++
+      }
+      else {
+        // advance by the number of designated bases
+        this.moveBaserunner(0, 0 + numBases)
+      }
+    }
   }
 
 
