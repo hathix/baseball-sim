e8eee513f923a0b5cae5254659cc0892e7d5dfb2
Neel Mehta <neelmehta96@gmail.com>
2018-10-28 19:12:06 -0700

    soft import vue bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BootstrapVue from 'bootstrap-vue';
 
 Vue.config.productionTip = false;
 
@@ -10,3 +11,8 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+// Vue.use(BootstrapVue);
+
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap-vue/dist/bootstrap-vue.css';
